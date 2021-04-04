610a78040d25192ae356c8402370372478eb7373
Stefanie-K <71380307+Stefanie-K@users.noreply.github.com>
2021-04-04 12:49:25 +0200

    ADD: view folder content

---
+++ src/views/Containers/TaskContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 6px 0;
+  width: 280px;
+  padding: 10px;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  border: 1px solid #ffffff26;
+`;
