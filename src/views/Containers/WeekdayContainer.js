f51add2aed898e0b17cd4a980b5baa301a2e26ae
StefanieKrohmann <steffi.krohmann@hotmail.com>
2021-04-04 13:25:44 +0200

    test
    test

--- src/views/Containers/WeekdayContainer.js
+++ src/views/Containers/WeekdayContainer.js
@@ -5,7 +5,7 @@ const WeekdayContainer = styled.div`
   margin: 6px 0;
   width: 280px;
   padding: 10px;
-  border-radius: 6px;
+  border-radius: 10px;
   display: flex;
   align-items: center;
   border: 1px solid #ffffff26;
