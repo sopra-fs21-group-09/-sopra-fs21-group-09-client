4b070da69ff6f923d5d3b27d149912fe2f4053f8
jnsgbl <jonasgebel08@gmail.com>
2021-04-07 15:39:45 +0200

    Working on registration page

--- src/views/Container.js
+++ src/views/Container.js
@@ -40,3 +40,30 @@ const CalendarEntryContainer = styled.div`
   align-items: center;
   border: 1px solid #ffffff26;
 `;
+
+// Contains the Logo in the registration and login page
+export const LogoContainer = styled.div`
+  height: ${props => props.height}px;
+  background: ${props => props.background};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+`;
+
+// Main light grey container for the login and registration page
+export const LoginMainContainer = styled.div`
+  height: ${props => props.height}px;
+  background: ${props => props.background};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
+  margin-top: 2%;
+  background: #E5E5E5;
+  margin-left: 30%;
+  padding-left: 15px;
+  margin-right: 30%;
+  padding-right: 15px;
+  border-radius: 20px;
+`;
