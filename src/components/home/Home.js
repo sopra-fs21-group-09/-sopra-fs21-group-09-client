42aa27d46a2cc0988eeb583b4850ffe7808892b2
StefanieKrohmann <71380307+StefanieKrohmann@users.noreply.github.com>
2021-04-08 16:50:16 +0200

    ADD css styles

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 import { BaseContainer } from '../../helpers/layout';
 import { api, handleError } from '../../helpers/api';
 import {withRouter} from "react-router-dom";
-import {RectButton, CircleButton} from "../../views/Button";
-import {GroupContainer, TaskContainer} from "../../views/Container";
+import {Button, RectButton, CircleButton, RoundButton, RectangleButton} from "../../views/Button";
+import {Weekday, WeekDayLabel, CalendarEntryContainer, GroupContainer, TaskContainer, WeekdayContainer, CalendarEntryTitle, CalendarEntry} from "../../views/Container";
 
 const Label = styled.label`
   color: white;
-  margin-bottom: 10px;
   text-transform: uppercase;
 `;
 
@@ -31,6 +30,11 @@ class Home extends React.Component {
                 <GroupContainer>
                 <Label>Home Group</Label>
                 </GroupContainer>
+                <TaskContainer><Label>Task</Label></TaskContainer>
+                <WeekdayContainer>
+                    <WeekDayLabel>Monday</WeekDayLabel>
+                    <CalendarEntry></CalendarEntry>
+                </WeekdayContainer>
             </BaseContainer>
         )
     }
--- src/views/Container.js
+++ src/views/Container.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {Component} from "react";
 import styled from "styled-components";
+import {BaseContainer} from "../helpers/layout";
+import {CircleButton, RectangleButton, RectButton, RoundButton} from "./Button";
+import "./design/styleSheet.css"
 
 export const GroupContainer = styled.div`
-  margin: 6px 0;
   border-radius: 6px;
   align-items: center;
   width: 15%;
@@ -15,30 +17,75 @@ export const GroupContainer = styled.div`
 
 export const TaskContainer = styled.div`
   margin: 6px 0;
-  width: 280px;
   padding: 10px;
   border-radius: 6px;
-  display: flex;
   align-items: center;
-  border: 1px solid #ffffff26;
+  width: 517px;
+  height: 48px;
+  left: 113px;
+  top: 432px;
+  background: #E5E5E5;
+  border: 3px solid #018692;
+  border-radius: 45px;
+  box-sizing: border-box;
 `;
 
 export const WeekdayContainer = styled.div`
-  margin: 6px 0;
-  width: 280px;
-  padding: 10px;
-  border-radius: 6px;
-  display: flex;
   align-items: center;
-  border: 1px solid #ffffff26;
+  width: 10%;
+  height: 816px; 
+  left: 170px;
+  top: 47px;
+  background: #FEFEFE;
+  border-radius: 30px;
+`;
+
+export const WeekDayLabel = styled.label`
+  margin: 20px 0px 10px 12px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  color: grey;
+  text-transform: uppercase;
 `;
 
 export const CalendarEntryContainer = styled.div`
   margin: 6px 0;
-  width: 280px;
+  width: 100%;
+  height: 80px;
   padding: 10px;
   border-radius: 6px;
-  display: flex;
   align-items: center;
   border: 1px solid #ffffff26;
+  background-color: #E86539;   
 `;
+
+export const CalendarEntryTitle = styled.label`
+  margin: 6px 0;
+`;
+
+export const CalendarEntryDescription = styled.label`
+  margin: 6px 0;
+  font-size: 10px;
+`;
+
+
+export class CalendarEntry extends Component {
+    render() {
+        return (<CalendarEntryContainer>
+                <CalendarEntryTitle>Title</CalendarEntryTitle>
+                <div className="new_line"/>
+                    <CalendarEntryDescription>Description</CalendarEntryDescription>
+            </CalendarEntryContainer>
+        )
+    }
+}
+
+export class Weekday extends Component {
+    render() {
+        return (<WeekdayContainer>
+                <WeekDayLabel>Monday</WeekDayLabel>
+            </WeekdayContainer>
+        )
+    }
+}
