7b6e3862dc78616d8a7002136edc295cec9c2e20
jnsgbl <jonasgebel08@gmail.com>
2021-04-12 09:12:23 +0200

    Updated User Class

--- src/components/profile/User.js
+++ src/components/profile/User.js
@@ -3,13 +3,19 @@
  */
 class User {
   constructor(data = {}) {
+
     this.id = null;
     this.username = null;
     this.token = null;
     this.status = null;
     this.password = null;
+    this.matrikel_nr = null;
+    this.color_coding = null
+
+    // These are not necessary at the moment
     this.creationDate = null;
     this.birthday = null;
+
     Object.assign(this, data);
   }
 }
