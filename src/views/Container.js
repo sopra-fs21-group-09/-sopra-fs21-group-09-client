0fdb0c82fe5f76b219549a05cfee8f28cd36e7cd
samuelewalzer <samuelewalzer@users.noreply.github.com>
2021-04-15 18:52:23 +0200

    EDIT home page

--- src/views/Container.js
+++ src/views/Container.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {HomeContainer} from "./layout";
 import {CircleButton, RectangleButton, RectButton, RoundButton} from "./Button";
 import "./design/styleSheet.css"
+import { COLORS } from "../views/design/colors"
 
 //GROUP
 export const GroupContainer = styled.div`
@@ -38,29 +39,14 @@ export class Group extends Component {
     }
 }
 
-//TASK
-export const TaskContainer = styled.div`
-  margin-right: 5px;
-`;
-
-export const Task = styled.div`
-  margin: 5px 0px 0px 0px;
-  width: 100%;
-  padding: 10px;
-  display: flex;
-  align-items: center;
-  background: #E5E5E5;
-  border: 3px solid #018692;
-  border-radius: 45px;
-`;
-
 //WEEKDAY
 export const WeekdayContainer = styled.div`
   align-items: center;
   height: 100%;
+  width: 80%;
   margin-right: 5px;
   padding: 5px 5px 0px 5px;
-  background: #FEFEFE;
+  background: #E5E5E5;
   border-radius: 20px;
   text-align: center;
 `;
@@ -75,7 +61,7 @@ export const WeekDayLabel = styled.label`
 export class Weekday extends Component {
     render() {
         return (<WeekdayContainer>
-                <WeekDayLabel>Monday</WeekDayLabel>
+                <WeekDayLabel></WeekDayLabel>
             </WeekdayContainer>
         )
     }
@@ -85,32 +71,44 @@ export class Weekday extends Component {
 export const CalendarEntryContainer = styled.div`
   &:hover {
     transform: translateY(-2px);
-    background: #EB8769;
+    background: ${COLORS.LECTURES};
   }
-  margin: 6px 0;
+  position: relative;
   width: 100%;
   height: 80px;
   padding: 10px;
+  margin: 5px 0px 5px 0px;
   border-radius: 5px;
-  display: flex;
   align-items: center;
-  background-color: #E86539; 
+  color: white;
+  background-color: ${COLORS.LECTURES};
 `;
 
 export const CalendarEntryTitle = styled.label`
   margin: 6px 0;
 `;
 
-export const CalendarEntryDescription = styled.label`
+export const CalendarDescription = styled.label`
   margin: 6px 0;
   font-size: 10px;
 `;
 
 export class CalendarEntry extends Component {
+    constructor() {
+      super();
+      this.state = {
+        top: null
+      }
+    }
+
+    calculatePosy(start, end) {
+      return end - start;
+    }
+
     render() {
         return (<CalendarEntryContainer>
-                <CalendarEntryTitle>Title</CalendarEntryTitle>
-                <CalendarEntryDescription>Description</CalendarEntryDescription>
+                <CalendarEntryTitle>{this.props.title}</CalendarEntryTitle>
+                <CalendarDescription>{this.props.description}</CalendarDescription>
             </CalendarEntryContainer>
         )
     }
@@ -120,44 +118,75 @@ export const CalendarContainer = styled.div`
   position: absolute;
   top: 100px;
   left: 100px;
-  height: 80%;
+  width: 70%;
+  height: 85%;
   display: flex;
   justify-content : space-around;
 `;
 
-export class Calendar extends Component {
-    render() {
-        return (<CalendarContainer>
-                    <Weekday>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                    </Weekday>
-                    <Weekday>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                    </Weekday>
-                    <Weekday>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                    </Weekday>
-                    <Weekday>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                    </Weekday>
-                    <Weekday>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                        <CalendarEntry></CalendarEntry>
-                    </Weekday>
-                </CalendarContainer>
-        )
-    }
-}
+export const DeadlineContainer = styled.div`
+  width: 100%;
+  height: 10%;
+  margin: 5px 0px 5px 0px;
+`;
 
+export const Deadline = styled.div`
+  &:hover {
+    transform: translateY(-2px);
+    background: #EB8769;
+  }
+  width: 100%;
+  height: 25px;
+  padding: 10px;
+  margin: 5px 0px 0px 0px;
+  border-radius: 5px;
+  float: left;
+  display: flex;
+  align-items: center;
+  background-color: ${COLORS.PRIVATE}; 
+`;
+
+export const UpcomingContainer = styled.div`
+  height: 50%;
+  margin: 0px 10px 0px 10px;
+`;
+
+export const Upcoming = styled.div`
+  &:hover {
+    transform: translateY(-2px);
+  }
+  width: 100%;
+  height: 25px;
+  padding: 10px;
+  margin: 5px 0px 5px 0px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  color: white;
+  background-color: ${COLORS.LECTURES}; 
+`;
+
+//TASK
+export const TaskContainer = styled.div`
+  margin: 0px 10px 0px 10px;
+  heigth: 50%;
+`;
+
+export const Task = styled.div`
+  &:hover {
+    transform: translateY(-2px);
+    background: ${COLORS.TASK};
+    color: white;
+  }
+  width: 100%;
+  padding: 5px 0px 5px 30px;
+  margin: 5px 0px 0px 0px;
+  border: 3px solid #018692;
+  border-radius: 45px;
+  display: flex;
+  align-items: center;
+  background: #E5E5E5;
+`;
 
 //LOGO
 // Contains the Logo in the registration and login page
--- src/views/design/colors.js
+++ src/views/design/colors.js
@@ -1,14 +1,13 @@
-
-//color scheme
-export const COLOR1 = '#E63d26' //deadlines
-export const COLOR2 = '#E86539' //lectures
-export const COLOR3 = '#F1A16A' //exercises
-export const COLOR4 = '#F3D4A9' //private
-export const COLOR5 = '#6AA49B' //meeting
-export const COLOR6 = '#018692'
-export const COLOR7 = '#026C8D'
-export const COLOR8 = '#0f224b' //button
-
-export const COLOR_BACKGROUND = '#F5F5F5'
-export const DARK_GREY = '#4F4F4F'
-export const LIGHT_GREY = '#E5E5E5'
+export const COLORS = {
+    DEADLINES : '#E63d26', //deadlines
+    LECTURES : '#E86539', //lectures
+    EXCERCISES : '#F1A16A', //exercises
+    PRIVATE : '#F3D4A9', //private
+    MEETING : '#6AA49B', //meeting
+    TASK : '#018692',
+    BLUE : '#026C8D',
+    BUTTON : '#0f224b', //button
+    BACKGROUND : '#F5F5F5',
+    DARK_GREY : '#4F4F4F',
+    LIGHT_GREY : '#E5E5E5', 
+}
