d9f3474ef7014c0992e401345323cf2dc7bcb70b
jnsgbl <jonasgebel08@gmail.com>
2021-04-15 16:54:38 +0200

    Profile Page nearly finished

--- src/views/design/colors.js
+++ src/views/design/colors.js
@@ -8,6 +8,11 @@ export const COLOR5 = '#6AA49B' //meeting
 export const COLOR6 = '#018692'
 export const COLOR7 = '#026C8D'
 export const COLOR8 = '#0f224b' //button
+export const COLOR9 = '#E0E0E0' //used in Profile.js
+export const COLOR10 = '#4F4F4F' //used in Profile.js
+export const COLOR11 = '#F5F5F5' //background from most pages
+export const COLOR12 = '#C4C4C4' //used in Profile.js
+export const COLOR13 = '#646464' //background of login and registration
 
 export const COLOR_BACKGROUND = '#F5F5F5'
 export const DARK_GREY = '#4F4F4F'
