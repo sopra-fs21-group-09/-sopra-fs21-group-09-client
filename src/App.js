846547ec65aa63a7837c59f70af21e705167318d
jnsgbl <jonasgebel08@gmail.com>
2021-04-27 18:29:06 +0200

    Added the command to start server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import AppRouter from "./helpers/Routing/routers/AppRouter";
 
+//Start the server with two terminals and the following two commands:
+// ./gradlew build --continuous or ./gradlew build --continuous -xtest
+// ./gradlew bootRun
+
 
 class App extends Component {
   render() {
