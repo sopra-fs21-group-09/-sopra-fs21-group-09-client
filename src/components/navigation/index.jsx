c2a4972fcf97b0591aeefa01cf3dd5ce50e49183
jnsgbl <jonasgebel08@gmail.com>
2021-04-30 09:36:50 +0200

    added id to localstorage + creating group works now

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -154,7 +154,7 @@ export function HamburgerMenu(props) {
           <BottomButton
               onClick={() =>
                 {history.push('/login');
-                localStorage.removeItem('token');}
+                localStorage.clear();}
               }
               initial={false}
               animate={isOpen ? "show" : "hide"}
