422c3abdacad755ecc4fa7473f94e3f7e89c4ddb
jnsgbl <jonasgebel08@gmail.com>
2021-05-06 09:47:00 +0200

    Easter Egg + Registration.js now Hook + Refactoring

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Colors } from "./design/Colors";
+import {Colors} from "./design/Colors";
 
 export const DESKTOP_WIDTH = 1160;
 export const SMALL_LAPTOPS_WIDTH = 970;
@@ -31,20 +31,3 @@ export const BaseContainer = styled.div`
   max-width: ${DESKTOP_WIDTH}px;
 `;
 
-// Main light grey container for the login and registration page
-export const LoginMainContainer = styled.div`
-  height: ${props => props.height}px;
-  background: ${props => props.background};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
-  margin-top: 2%;
-  margin-bottom: 4%;
-  background: #E5E5E5;
-  margin-left: 30%;
-  padding-left: 15px;
-  margin-right: 30%;
-  padding-right: 15px;
-  border-radius: 20px;
-`;
