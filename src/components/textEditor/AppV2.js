15d0ec1d5c6089fba85c810e3bd6132486bc6fb0
Loris De Luca <loris.deluca@uzh.ch>
2021-05-23 18:58:23 +0200

    8===D

---
+++ src/components/textEditor/AppV2.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import { Editor, EditorState, convertToRaw, convertFromRaw } from 'draft-js'
+import SockJsClient from "react-stomp";
+import j from 'jsondiffpatch'
+
+class AppV2 extends Component {
+  state = {
+    editorState: EditorState.createEmpty(),
+    id: "test"
+  }
+
+  handleMessage = (data) => {
+    if (!data) return
+    let nextContentState = convertFromRaw(data)
+    this.setState({
+      editorState: EditorState.push(this.state.editorState, nextContentState)
+    })
+  }
+
+  sendMessage = (editorState) => {
+    this.clientRef.sendMessage('/app/user-all', JSON.stringify({
+      name: this.state.id,
+      message: JSON.stringify(convertToRaw(editorState.getCurrentContent()))
+    }));
+  };
+
+  onChange = editorState => {
+    this.sendMessage(editorState)
+    this.setState({ editorState })
+  }
+
+  render () {
+    return (
+        <div>
+          <Editor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+          />
+          <SockJsClient url='http://localhost:8080/websocket-chat/'
+                        topics={['/topic/user']}
+                        onConnect={() => {
+                          console.log("connected");
+                        }}
+                        onDisconnect={() => {
+                          console.log("Disconnected");
+                        }}
+                        onMessage={(msg) => {
+                          this.handleMessage(JSON.parse(msg.message))
+                          console.log(JSON.parse(msg.message))
+                        }}
+                        ref={(client) => {
+                          this.clientRef = client
+                        }}/>
+        </div>
+    )
+  }
+}
+
+export default AppV2;
